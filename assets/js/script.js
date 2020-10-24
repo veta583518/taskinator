e7cd5b27ad3d1563182b4020aa4f3b53ff3dd204
CoCo Chanel <veta583518@gmail.com>
2020-10-23 21:04:58 -0500

    Add task type to the list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,28 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
 
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+    
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-  
 }
 
-// Create Li element when save task button is clicked.
+// Create Li element
 formEl.addEventListener("submit",createTaskHandler);
 
 

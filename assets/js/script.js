b14bbe9c47af36d0c42eeff0c76fdef1d60a54be
CoCo Chanel <veta583518@gmail.com>
2020-10-23 21:47:10 -0500

    refactored code to split createTaskHandler into two seperate functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,37 @@ var createTaskHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    console.log(taskTypeInput);
     
+    // Package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // Send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+
+};
+
+// Create new task HTML element
+var createTaskEl = function (taskDataObj) {
+
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     // give it a class name
     taskInfoEl.className = "task-info";
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-}
 
+}
 // Create Li element
 formEl.addEventListener("submit",createTaskHandler);
 

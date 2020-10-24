4ac1c26df0547e7ba81f435f2b7a5b1db1f56186
CoCo Chanel <veta583518@gmail.com>
2020-10-23 20:20:24 -0500

    Updated the event Listener to submit from onclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
-var buttonEl = document.querySelector("#save-task")
+// Variable Declarations
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-// Task Handler to create task item
 
-var createTaskHandler = function () {
+// Task Handler to create task item
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+  
 }
 
 // Create Li element when save task button is clicked.
-buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit",createTaskHandler);
 
 
